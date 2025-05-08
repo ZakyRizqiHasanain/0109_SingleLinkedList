cf3bb523e0a68bfa56700e5eec50de87f3557aa6
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-05-08 15:25:38 +0700

    function untuk menjalankan current dan previous agar mencari data yang sesuai

--- algos.cpp
+++ algos.cpp
@@ -38,5 +38,13 @@ class list{
         node *previous = start;
         node *current = start;
 
+        while ((current != NULL) && (nim >= current->noMhs)){
+            if (nim == current->noMhs){
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
     }
     };
