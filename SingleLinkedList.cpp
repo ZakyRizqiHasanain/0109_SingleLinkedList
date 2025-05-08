6f1cb28b1caba9f9b150b6baddf29352b8d1f9f0
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-05-08 16:28:28 +0700

    selesai

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -102,15 +102,78 @@ int main(){
     list mhs;
     int nim;
     char ch;
-    while (1){
+    do{
         cout << "Menu" << endl;
         cout << endl
              << "1. Menambahkan data kedalam list" << endl
              << "2. Menghapus data dari dalam list" << endl
              << "3. Menampilkan semua data dialam list" << endl
              << "4. Mencari data dalam list" << endl
-             << "5. Keluar" ;
+             << "5. Keluar" << endl;
         cout << "Masukkan pilihan (1-5): ";
         cin >> ch;
-    }
+        switch (ch){
+            case '1':
+            {
+                mhs.addnode();
+                break;
+            }
+            case '2':
+            {
+                if (mhs.listempty())
+                {
+                    cout << endl
+                         << "List Kosong" << endl;
+                         break;
+                }
+                cout << endl
+                     << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                     cin >> nim;
+                     if (mhs.delnode(nim) == false)
+                     cout << endl
+                          << "Data tidak ditemukan" << endl;
+                    else 
+                    cout << endl
+                         << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus" << endl;
+            }
+            break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
+            case '4':
+            {
+                if (mhs.listempty() == true)
+                {
+                    cout << "\nList Kosong";
+                    break;
+                }
+                node *previous, *current;
+                cout << endl
+                     << "Masukkan no mahasiswa yang di cari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                         << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
+            case '5':
+            {
+            }
+            break;
+            default:
+            {
+                cout << "Pilihan salah !." << endl;
+            }
+            break;
+        }
+    }while (ch != '5');
 }
